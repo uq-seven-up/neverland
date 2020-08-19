4ddcb366650e2128fdd09707e2142f6327272ca7
mscherzer@gmail.com <mscherzer@gmail.com>
2020-08-19 11:40:09 +1000

    enable cors headers to allow cross domain ajax calls to the noe server.

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -4,6 +4,12 @@ import screenRouter = require('./routes/screen');
 const app: express.Application = express();
 const PORT = 3080;
 
+app.all('*', function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type,authorization');
+	next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/screen',screenRouter);

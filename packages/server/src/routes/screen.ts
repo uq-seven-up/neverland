835400fef1d2d3554f7cd3db2019ff6fd58e0086
mscherzer@gmail.com <mscherzer@gmail.com>
2020-08-19 12:25:57 +1000

    add rss reader dependency, add rest endpoint which fetches news from the UQ news RSS feed.

--- packages/server/src/routes/screen.ts
+++ packages/server/src/routes/screen.ts
@@ -1,19 +1,35 @@
-import express = require('express');
+import * as express from 'express';
+import { Request, Response } from "express";
+import RSSParser = require('rss-parser');
 
 const router = express.Router();
 
-router.get('/test:id',(req:any,res:any) => {
+router.get('/test:id',(req:Request,res:Response) => {
 	const {id} = req.params;
 	res.send({id})
 });
 
-router.get('/test',(req:any,res:any) => {
+router.get('/test',(req:Request,res:Response) => {
 	res.send('{message:"Hello World"}')
 });
 
-router.post('/test',(req:any,res:any) => {
+router.post('/test',(req:Request,res:Response) => {	
 	// Echo back the response body.
 	res.send(req.body)
+});
+
+router.get('/uqnews',async(req:Request,res:Response) => {
+	let rssParser = new RSSParser();
+	try{		
+		await rssParser.parseURL('https://www.uq.edu.au/news/rss/news_feed.xml',(err,feed) => {
+		if (err) throw err;
+		res.send(feed)
+	});
+	} catch(error)
+	{
+		console.log('Some error occured fetching data from the news feed.')
+		express.response.sendStatus(500)
+	}
 });
 
 export = router;

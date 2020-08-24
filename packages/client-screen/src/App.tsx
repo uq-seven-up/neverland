dfd4a1bd3264d5ff91aca20e9032330cd0db4c33
gdownunder <69148433+gdownunder@users.noreply.github.com>
2020-08-24 20:51:44 +1000

    ClockWidget added to WidgetContainer

--- packages/client-screen/src/App.tsx
+++ packages/client-screen/src/App.tsx
@@ -4,6 +4,7 @@ import WeatherWidget from './component/WeatherWidget'
 import WidgetContainer from './component/WidgetContainer'
 import RssWidget from './component/RssWidget'
 import TestWidget from './component/TestWidget';
+import ClockWidget from './component/ClockWidget';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 		<TestWidget name="UQ Lakes"/>
 		<TestWidget name="UQ Chancellor's Place"/>
 		<RssWidget />
+		<ClockWidget name="Clock" />
 	</WidgetContainer>
   );
 }

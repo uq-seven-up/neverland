3b5915f76938cabd77c818b3bbf45eef6013cfb8
gdownunder <69148433+gdownunder@users.noreply.github.com>
2020-08-25 14:35:53 +1000

    clock updated

--- packages/client-screen/src/component/ClockWidget.tsx
+++ packages/client-screen/src/component/ClockWidget.tsx
@@ -1,66 +1,46 @@
-import React from "react"
+import React from 'react'
+import { useState, useEffect } from 'react';
 
 interface ClockWidgetProp {
   name: string
 }
 
-interface ClockWidgetState {
-  
-}
-
 /**
- * This widget is a proof of concept implementation of a 
- * react component using a class.
+ * Clock widget for the big screen
  */
-class ClockWidget extends React.Component<ClockWidgetProp, ClockWidgetState> {  
-    constructor(props: any) {
-        super(props)
-
-        
-    }
-
-    /* ########################################################*/
-    /* React life-cycle event.*/
-    public componentDidMount(): void {
-        console.log('Component Did Mount')
-    }
-    /* ########################################################*/
-
+function ClockWidget(props: any) {    
+    const [date, setDate] = useState(new Date());
   
+   //Replaces componentDidMount and componentWillUnmount
+   useEffect(() => {
+    var timerID = setInterval( () => tick(), 1000 );
   
-    /* ########################################################*/
-    /* UI Rendering*/
-    /**
-     * Render a sub-component based on some business logic. Just another proof
-     * of concept to see how the main render method can use helper methods for 
-     * modularising rendering the component HTML.
-     * 
-     * @returns JSX element
-     */
-    private renderSubComponentFoo() {
 
+    //Clear memory
+    return function cleanup() {
+        clearInterval(timerID);
+      };
+   });
 
-        return (
-        <div>
-
-        </div>
-        )
-    }
-
-    public render() {
-        return (
+   //Set time
+   function tick() {
+    setDate(new Date());
+   }
+     
+   //Return time
+    return (
 		<section className="widget clock">
         	<div className="heading">
-				<h2>{this.props.name}</h2>
+				<h2>{props.name}</h2>
 				<figure></figure>
 			</div>
-
+        
 			<div className="content">
-            	{this.renderSubComponentFoo()}
+                <p>{date.toLocaleTimeString()}</p>
 			</div>
         </section>
         )
-    }
 }
+        
 
 export default ClockWidget

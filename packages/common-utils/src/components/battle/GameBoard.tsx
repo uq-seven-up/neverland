602e6265accef41160c469b44f40c96b175e411a
Marcel <marcel@cfavatar.com>
2020-09-20 16:45:04 +1000

    added global widget which renders a game board.

---
+++ packages/common-utils/src/components/battle/GameBoard.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import {GameMap} from '@7up/common-types';
+
+
+
+interface GameBoardProp {
+	rows:number, /* The game boards row count. */
+	cols:number,  /* The game boards column count. */
+	gameMap:GameMap
+}
+
+/**
+ * Renders the game board.
+ */
+class GameBoard extends React.Component<GameBoardProp> {
+	/**
+	 * Render one row of the game board.
+	 */
+	private createRow(row,n) {
+		var td = [];
+		for(var i = 0; i < n; i++){
+			let id = "g_" + i + '_' + row;
+			let className = null;
+			let tile = this.props.gameMap.get(id);
+			if(tile)
+			{
+				className = tile.class;
+			}
+			
+			td.push(<td key={id} id={id} className={className}>{`${i}, ${row}`}</td>)
+		}
+
+		return (<tr key={`r_${row}`}>{td}</tr>)
+	}
+
+	public render() {		
+		var rows = [];
+		for(var i = 0; i < this.props.rows; i++){
+			rows.push(this.createRow(i,this.props.cols));
+		}
+				
+		return(
+		<div className="gameMap">
+        	<table>
+				<tbody>
+					{rows}
+				</tbody>			
+			</table>						
+		</div>
+        )
+    }
+}
+
+export default GameBoard;
--- packages/common-utils/src/index.tsx
+++ packages/common-utils/src/index.tsx
@@ -2,3 +2,4 @@ export {API} from "./lib/Api";
 export * from "./lib/CFKitUtil";
 export { default as getFirstDayLastDayUTC } from "./components/getFirstDayLastDayUTC";
 export { default as isFlagSet } from "./components/flags";
+export { default as GameBoard } from "./components/battle/GameBoard";

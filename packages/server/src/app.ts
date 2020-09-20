50d74122f467bb04c20afbafc25bd7675a3edf62
Marcel <marcel@cfavatar.com>
2020-09-20 12:42:31 +1000

    implement a web socket server, implement a proof of concept example endpoint for broadcasting messages to web socket clients from a REST endpoint.

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,18 +1,21 @@
 import express = require('express');
 import { Request, Response } from 'express';
+import WebSocket from 'ws';
+
+import {DB} from './controller/db';
+import busRoutes = require('./routes/bus');
 import exampleRoutes = require('./routes/example');
 import pollRoutes = require('./routes/poll');
 import screenRoutes = require('./routes/screen');
-import busRoutes = require('./routes/bus');
 import StudySpaceRoutes = require('./routes/study-space');
-import { DB } from './controller/db';
 
 const app: express.Application = express();
 const PORT = 3080;
 
-/* Establish a connection to mongodb on server start,*/
+/* Establish a connection to MongoDb. (By instantiating an arbitratry DB model.) */
 new DB.Models.RssFeed();
 
+/* Set headers to allows cross origin resource sharing (CORS) for the exposed REST API. */
 app.all('*', function (req: Request, res: Response, next: any) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
@@ -21,12 +24,28 @@ app.all('*', function (req: Request, res: Response, next: any) {
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+/* Register REST routes. */
 app.use('/api/example', exampleRoutes);
 app.use('/api/poll', pollRoutes);
 app.use('/api/screen', screenRoutes);
 app.use('/api/bus', busRoutes);
 app.use('/api/studyspace', StudySpaceRoutes);
 
-app.listen(PORT, function () {
+const server = app.listen(PORT, function () {
 	console.log(`Express is listening on port ${PORT}`);
+});
+
+/* Configure websocket server. (attached to local scope, to make the ws serve accessible within routes.) */
+app.locals.ws = new WebSocket.Server({noServer:true,clientTracking:true}) as WebSocket.Server;
+app.locals.ws.on('connection', (socket : WebSocket) => {
+	socket.on('message', message => {
+		console.log(message)
+	});
+});
+
+server.on('upgrade', (request, socket, head) => {
+	app.locals.ws.handleUpgrade(request, socket, head, (socket:any) => {
+		app.locals.ws.emit('connection', socket, request);
+	});
 });
--- packages/server/src/routes/example.ts
+++ packages/server/src/routes/example.ts
@@ -1,19 +1,7 @@
 import * as express from 'express';
 import { Request, Response } from "express";
 import dotenv from "dotenv";
-import {DB} from '../controller/db'
-import {IBusTime} from '../models/bus-time';
-
-const fs = require('fs');
-
-interface BusTimeSource {
-	stop_id: number,
-	route_id: string;
-    trip_id: string;
-    departure_time: string;
-    trip_headsign: string;
-}
-
+import WebSocket from 'ws';
 
 /**
  * Defines routes which are intended to be used to provide 
@@ -27,94 +15,27 @@ dotenv.config();
  * it to a variable.
  */
 router.get('/test/:id',(req:Request,res:Response):void => {
-	const {id} = req.params;
+	const {id} = req.params;	
 	res.send({id})
 });
 
 
 /**
- * Proof of concept for receiveing an JSON object in the request body and 
- * echoing the JSON back in the response.
+ * Proof of concept for pushing a message to all connected web socket clients 
+ * in response to a REST call.
  */
-router.post('/test',async (req:Request,res:Response) => {	
-	// Echo back the response body.
-	let rawData = fs.readFileSync('E:\\translink.json');
-	let translinkData = JSON.parse(rawData);
-	console.log(translinkData.data.length);
+router.get('/broadcast',async(req:Request,res:Response) => {
+	req.app.locals.ws.clients.forEach(function each(client:any) {
+		if (client.readyState === WebSocket.OPEN) {
+			let data = {
+				message:"Hello from node express!"
+			}
+			
+			client.send(JSON.stringify(data));
+		}
+	});
 	
-	// for(var i=0; i < translinkData.data.length; i++)
-	// {
-	// 	let src = translinkData.data[i];		
-	// 	let busTime = new DB.Models.BusTime();
-	// 	busTime.stop_id = Number(src.stop_id);
-	// 	busTime.route_id = src.route_id;
-	// 	busTime.trip_id = src.trip_id;
-	// 	busTime.departure_time = src.departure_time;
-	// 	busTime.trip_headsign = src.trip_headsign;
-
-	// 	await busTime.save((error:any,object:IBusTime) => {		
-	// 		if(error){
-	// 			console.log(error)
-	// 			res.status(500).send({success:false,'msg':'Error creating poll.'})
-	// 			return;
-	// 		}			
-	// 	});
-	//}
-	
-
-	res.send({success:true,data:'done'})
+	res.send({results:'Completed Broadcast to connected clients.'})
 });
-
-// router.post('/mongo/example',async(req:Request,res:Response) => {	
-// 	Echo back the response body.
-// 	let contact = new DB.Models.Example(
-// 		{
-// 			name:"jack",
-// 			email:"jack@work.com",
-// 			phone:"1234567",
-// 			message:"Testing mongoose example."
-// 		}
-// 	);
-	
-// 	try {
-// 		await contact.save((err:any) => {
-// 			if (err) throw err;
-// 			res.send({sucess:true,message:"Saved example to mongodb."})
-// 		})	
-// 	} catch (error) {
-// 		console.log(error);
-// 		express.response.sendStatus(500)
-// 	}
-// });
-
-router.get('/foo',async(req:Request,res:Response) => {
-	const FILTER = {
-		"trip_id" : /.*Weekday./,
-		"stop_id":{$in:[1853,1797]}
-	}
-	
-	try {
-		await DB.Models.BusTime.find(FILTER, (err, results) => {
-			if (err) throw err;
-			res.send({results})
-		})	
-	} catch (error) {
-		console.log(error);
-		express.response.sendStatus(500)
-	}
-});
-
-// router.get('/mongo/example/:id',async(req:Request,res:Response) => {
-// 	const {id} = req.params;
-// 	try {
-// 		await DB.Models.Example.findById(id,(err, results) => {
-// 			if (err) throw err;
-// 			res.send({results})
-// 		})	
-// 	} catch (error) {
-// 		console.log(error);
-// 		express.response.sendStatus(500)
-// 	}
-// });
 
 export = router;

a6f80f7bde55c00dc08ffb405b3064e8bfeb17a5
Marcel <marcel@cfavatar.com>
2020-09-20 16:43:24 +1000

    add a global type definition for game board data.

--- packages/common-types/src/types/GlobalTypes.ts
+++ packages/common-types/src/types/GlobalTypes.ts
@@ -4,39 +4,11 @@ export interface Timezone {
     name:string,
 }
 
-export interface Image {
-    state:number,
-    name:string,
-    src:string,
-    thumb:string
+export interface GameTile{
+	id:string,
+	x:number,
+	y:number,
+	class:string
 }
 
-export interface Coordinate {
-    lat:number,
-    lng:number
-}
-
-export interface Identity {
-    guid:string,
-    name:string,
-}
-
-export interface SearchFilter {
-    term:string,
-    page:number,
-    rows:number,
-    sidx:string,
-    sord:"asc"|"desc"
-}
-
-export interface RecordSetMeta  
-{
-    /** Current page. */
-    page:number,
-    /** Total number of pages. */
-    pageCount:number,
-    /** Number of records on the current page. this may be smaller than requested page size. (e.g. on the first or last page) */
-    count:number,
-    /** Total number of records found accross all pages. */
-    recordCount:number
-}
+export type GameMap = Map<string,GameTile>;

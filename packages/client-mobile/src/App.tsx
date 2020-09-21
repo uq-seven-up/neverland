4735565c01c66f76a485d1c3eb9b149ab1a12b72
gdownunder <69148433+gdownunder@users.noreply.github.com>
2020-09-21 22:41:14 +1000

    Mobile menu w/o home button

--- packages/client-mobile/src/App.tsx
+++ packages/client-mobile/src/App.tsx
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import PollView from './component/PollView'
 import FrontPage from './component/FrontPage'
 import Menu from './component/Menu'
+import GameClient from './component/GameClient'
 
-function App() {
-  return (
-    <>
-    <FrontPage />
+class App extends Component {
+render() {
+return(
+  <BrowserRouter>
+  <div>
     <Menu />
-    {/* <PollView></PollView>  */}
-    </>
-  );
+    <Switch>
+      <Route path="/" component={FrontPage} exact/>
+      <Route path="/PollView" component={PollView} />
+      <Route path="/GameClient" component={GameClient} />
+    </Switch>
+  </div>
+  </BrowserRouter>
+)
 }
+}
+
+
+// function App() {
+//   return (
+//     <>
+
+//     <FrontPage />
+//     <Menu />
+
+//     <Switch>
+//       <Route exact path="/PollView" component ={PollView} />
+//       <Route exaxt path="/">
+//         <Redirect to="PollView" />
+//       </Route>
+//       <Route exact path="/GameClient" component={GameClient} />
+//     </Switch>
+    
+//     </> 
+//   );
+// }
 
 
 export default App;

be7ee7834638275237ae848ae6843b470efbd58e
gdownunder <69148433+gdownunder@users.noreply.github.com>
2020-09-21 13:00:45 +1000

    mobile screen

---
+++ packages/client-mobile/src/component/Menu.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+function Menu(){
+
+return (
+
+<section className='section2'>
+    
+    <div className='box4'>
+        <figure></figure>
+    </div>
+
+    <div className='box5'>
+        <figure></figure>
+    </div>
+    
+</section>
+)
+}   	
+
+export default Menu

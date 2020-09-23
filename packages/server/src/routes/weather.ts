34388d6206c906cd3048be6ac7fb6d280d2982b7
Marcel <marcel@cfavatar.com>
2020-09-23 22:31:53 +1000

    debug env variable

--- packages/server/src/routes/weather.ts
+++ packages/server/src/routes/weather.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import {DB} from '../controller/db'
 import { IWeather } from '../models/weather';
 import fetch from "node-fetch";
-
+//const fetch = require('node-fetch');
 
 
 /**
@@ -49,14 +49,14 @@ router.get('/weather', async (req: Request, res: Response) => {
 
 	try {
 		await fetch(API_URL)
-			.then(res => res.json())
-			.then(result => {
+			.then((res:any) => res.json())
+			.then((result:any) => {
 				weatherData!.temp = result.main.temp as number;
 				weatherData!.status = result.weather[0].main as string;
 			});
 	} catch (error) {
-		console.log('Some error occured fetching data from the Weather API.')
-		res.status(500).send({ success: false, 'msg': 'Error fetching Weather data.' })
+		console.log('Some error occured fetching data from the Weather API.',API_URL)
+		res.status(500).send({ success: false, 'msg': 'Error fetching Weather data.'})
 		
 	}
 

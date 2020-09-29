35c8c0d528c12302dcb92f644b286c39ae10efa9
marcel <marcel@cfavatar.com>
2020-09-29 23:59:49 +1000

    game messages are now piped through a game server object which manages a player queue.

--- packages/server/src/lib/GameServer.ts
+++ packages/server/src/lib/GameServer.ts
@@ -1,3 +1,54 @@
+import WebSocket from 'ws';
+
+/** 
+Message Protocol
+
+To optimise speed and bandwidth the game server does NOT use JSON format for data transfer. Instead 
+messages are a pipe delimited string.
+
+Position: 
+0 = g : Message for the game.
+	c : Message for the controller (mobile client)
+	b : Broadcast message to all controllers (mobile client)
+1 = Game command:
+	j : Request to join game.
+	n : Move player north.
+	ne: Move player north east.
+	e : Move player east.
+	se: Move player south east.
+	s : Move player south.
+	sw: Move player south west.
+	w : Move player west.
+	nw: Move player north west.
+	h : Stop player.
+	x : Exit game.
+	v: Game event.
+2 = Identifier for the websocket. When connecting the identifier is passed as the url
+	parameter 'uuid' when openeing the connection. (ignored for can be empty for broadcast messages.)
+3 = Event type.
+	100: Game Ended
+	200: Collected an item
+
+
+Example Message:
+Notify the game that the player connect to socket uuid: WER12334PSK should move north.
+
+g|n|WER12334PSK 
+
+Notify the controller used by player on socket uuid WER12334PSK that the player picker up a candy.
+
+c|v|WER12334PSK|200
+
+Notify all game controllers that the game is over.
+b|v||100
+
+*/
+
+export interface GameWebSocket extends WebSocket
+{
+	uuid:string
+}
+
 interface PlayerData {
     uuid:string,
     name:string,
@@ -7,16 +58,13 @@ interface PlayerData {
 export class GameServer
 {
     static MAX_PLAYERS = 4
-    private _count:number
     private _queue:Map<string,PlayerData>
     private _field:PlayerData[]
     private _state:"running"|"waiting"|"unknown"
     
     constructor() {
-        this._count = 0;
         this._state = "unknown";
         this._queue = new Map();
-
         this._field = []
     }
 
@@ -38,10 +86,15 @@ export class GameServer
         let i = 0;
         this._field = [];
         for (let player of this._queue.values()) {
-            player.inField = true;
-            this._field.push(player);
+            this.addPlayerToField(player);
             if(this._field.length === GameServer.MAX_PLAYERS) break;
         }
+    }
+
+    private addPlayerToField(player:PlayerData)
+    {
+        player.inField = true;
+        this._field.push(player);
     }
 
     public nextGame()
@@ -79,35 +132,84 @@ export class GameServer
 
     public join(uuid:string,name:string)
     {
-       
+       /* Ensure that the state/position of a player in the queue is not changed if 
+          re-connecting. */
        if(!this._queue.has(uuid))
        {
-           this._queue.set(uuid,{uuid:uuid,name:name,inField:false})
-           this._count++;
+           this._queue.set(uuid,{uuid:uuid,name:name,inField:false});
+           console.log('Player added to queue.');
        }
 
-       let playerData = this._queue.get(uuid);
-       if(playerData?.inField) return;
+       /* The player is in game already.*/
+       let player = this._queue.get(uuid);
+       if(player?.inField) return;
 
-       if(this._field.length === GameServer.MAX_PLAYERS){
+       /* The game is currently full. */
+       if(this._field.length >= GameServer.MAX_PLAYERS){
            console.log('Send game, full placed in queue.');
            return;
        }
 
-       if(this._state === 'running'){
-        console.log('Game in progress, please wait.');
-        return;
+       if(player)
+       {
+           this.addPlayerToField(player);
+           console.log('Player added to the game.');
        }
-
-       this._field
-
-        
-        // Is spot ready or reconnecting?
-        // start routing messages.
-        // is game waiting
-        //    sen
-        // else
-        // send queued message
-
     }
+
+    /**
+	 * Re-route an incoming game message to a specific mobile client. 
+	 * @param ws Reference to the web socket server.
+	 * @param message (See game message protocol.)
+	 */
+	public routeGameMessage(ws:any,message:string):void
+	{						
+        let data = message.split('|');
+		ws.clients.forEach(function each(client:GameWebSocket) {
+			if (client.uuid === data[2] && client.readyState === WebSocket.OPEN) {			
+				client.send(message);
+			}
+		});
+	}
+
+	/**
+	 * Broadcast an incoming game message to all connected mobile clients. 
+	 * @param ws Reference to the web socket server.
+	 * @param message (See game message protocol.)
+	 */
+	public broadCastGameMessage(ws:any,message:string):void
+	{						
+		ws.clients.forEach(function each(client:GameWebSocket) {
+			if (client.uuid !== 'GAME_SCREEN' && client.readyState === WebSocket.OPEN) {			
+				client.send(message);
+			}
+		});
+	}
+
+	/**
+	 * Send a game message directly to the game screen. 
+	 * @param ws Reference to the web socket server.
+	 * @param message (See game message protocol.)
+	 */
+	public sendToGameScreen(ws:any,message:string,from:string):void
+	{						
+        let data = message.split('|');
+        if(data[1] === 'j')
+        {
+            this.join(from,'todo assign playername');
+        }
+
+        if(!this._queue.has(from) || this._queue.get(from)!.inField === false)
+        {
+            console.log('Message blocked player not on field.')
+            return;
+        } 
+
+        /* Note, in this context the client.uuid is the sending sockets uuid. */
+        ws.clients.forEach(function each(client:GameWebSocket) {
+            if (client.uuid === 'GAME_SCREEN' && client.readyState === WebSocket.OPEN) {			
+				client.send(message);
+			}
+		});
+	}
 }
